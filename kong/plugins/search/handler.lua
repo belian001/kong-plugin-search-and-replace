86e3bec5aab33daa8b6aa71e778eb06f332a817f
wyq007 <1653077113@qq.com>
2022-12-21 19:02:00 +0800

    serarch and wirter

--- kong/plugins/search/handler.lua
+++ kong/plugins/search/handler.lua
@@ -4,7 +4,25 @@ local plugin = {
 }
 
 
+
 function plugin:body_filter(plugin_conf)
+  local chunk, eof = ngx.arg[1], ngx.arg[2]
+  local ctx = ngx.ctx
+
+  ctx.rt_body_chunks = ctx.rt_body_chunks or {}
+  ctx.rt_body_chunk_number = ctx.rt_body_chunk_number or 1
+  if eof then
+    local body = concat(ctx.rt_body_chunks)
+    if (string.find(body, plugin_conf.response_search_string) and string.find(body, "Agent"))  then
+
+      ngx.arg[1] = plugin_conf.response_replace_string
+  else
+    ctx.rt_body_chunks[ctx.rt_body_chunk_number] = chunk
+    ctx.rt_body_chunk_number = ctx.rt_body_chunk_number + 1
+    ngx.arg[1] = nil
+  end
+end 
+--[[function plugin:body_filter(plugin_conf)
   if kong.ctx.plugin.dont_process then
     return
   end
@@ -19,6 +37,6 @@ function plugin:body_filter(plugin_conf)
       end
     end
   end
-end 
+end ]]
 
 return plugin
