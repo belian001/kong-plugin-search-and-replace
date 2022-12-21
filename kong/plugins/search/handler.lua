0b6d76c8ab995ea084cdddf64bf1306d3a7f659b
wyq007 <1653077113@qq.com>
2022-12-21 19:04:09 +0800

    serarch and wirter

--- kong/plugins/search/handler.lua
+++ kong/plugins/search/handler.lua
@@ -22,21 +22,5 @@ function plugin:body_filter(plugin_conf)
     ngx.arg[1] = nil
   end
 end 
---[[function plugin:body_filter(plugin_conf)
-  if kong.ctx.plugin.dont_process then
-    return
-  end
-  if plugin_conf.response_search_string then
-    local body = kong.response.get_raw_body()
-    if body then
-      kong.log.debug("Response search string: " .. plugin_conf.response_search_string)
-      local replace_string = plugin_conf.response_replace_string
-      if string.find(body, plugin_conf.response_search_string) then
-        kong.log.debug("Response writer string: " .. replace_string)
-        kong.response.set_raw_body(replace_string)
-      end
-    end
-  end
-end--]]
 
 return plugin
